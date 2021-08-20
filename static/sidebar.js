d7c965259a23a888dae6231b1601692094915c59
ivanbaug <ivsitioinfo@gmail.com>
2021-08-19 22:05:34 -0500

    Initial commit

---
+++ static/sidebar.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+});
+$(document).ready(function () {
+
+  $('#sidebarCollapse-right').on('click', function () {
+      $('#sidebar-right').toggleClass('active');
+  });
+
+});
