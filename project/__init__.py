c7fc6077653c4f6a3039bfaaa77a869ac3dd74b7
ivanbaug <ivsitioinfo@gmail.com>
2021-08-25 22:09:37 -0500

    start working on the user db

--- project/__init__.py
+++ project/__init__.py
@@ -23,6 +23,10 @@ def create_app(config_file='config.py'):
 
     from .models import User
 
+    ### 2 Lines below only required once, when creating DB. ####
+    # with app.app_context():
+    #     db.create_all()
+
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
