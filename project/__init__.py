22fc0b6fc259cde91973a1763e7234ed9d8f6125
ivanbaug <ivsitioinfo@gmail.com>
2021-12-14 08:07:03 -0500

    add dockerfile and clean requirements.txt

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import os
+
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
